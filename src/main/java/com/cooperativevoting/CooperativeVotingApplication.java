cc7fe5929d9a3e1b2057273b3132ce9480003dbd
MatusMoura2 <matus.moura333x2@gmail.com>
2026-09-17 23:32:23 -0300

    refactor: rename project to cooperative-voting-api

---
+++ src/main/java/com/cooperativevoting/CooperativeVotingApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativevoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeVotingApplication.class, args);
+	}
+
+}
