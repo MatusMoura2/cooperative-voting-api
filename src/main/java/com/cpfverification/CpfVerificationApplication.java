efb9e3b833ab9186e46f383ed58cbf13190bf0ad
MatusMoura2 <matus.moura333x2@gmail.com>
2026-09-17 23:27:35 -0300

    chore: setup hexagonal architecture package structure

---
+++ src/main/java/com/cpfverification/CpfVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.cpfverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpfVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpfVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpfverification/cpf_verification/CpfVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpfverification.cpf_verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
